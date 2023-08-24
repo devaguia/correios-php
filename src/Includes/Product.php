5d9ae360cbc642b84a27bb979f1210134d707022
Matheus Aguiar <aguiartgv@gmail.com>
2023-08-24 18:47:08 -0300

    test: creating product unit tests

--- src/Includes/Product.php
+++ src/Includes/Product.php
@@ -4,4 +4,35 @@ namespace Correios\Includes;
 
 class Product
 {
+    private int $weight;
+    private float $width;
+    private float $height;
+    private float $length;
+    public function __construct(int $weight, float $width, float $height, float $length)
+    {
+        $this->weight = $weight;
+        $this->width  = $width;
+        $this->height = $height;
+        $this->length = $length;
+    }
+
+    public function getWeight(): int
+    {
+        return $this->weight;
+    }
+
+    public function getWidth(): float
+    {
+        return $this->width;
+    }
+
+    public function getHeight(): float
+    {
+        return $this->height;
+    }
+
+    public function getLength(): float
+    {
+        return $this->length;
+    }
 }
