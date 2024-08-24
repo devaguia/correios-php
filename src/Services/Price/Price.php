276831e94c37ffe9e0d11bcade386180e5fad62a
Rafael Corrêa Gomes <rafaelstz@users.noreply.github.com>
2024-08-24 13:08:00 -0400

    Payload em pt-br para estimar preço

--- src/Services/Price/Price.php
+++ src/Services/Price/Price.php
@@ -55,23 +55,23 @@ class Price extends AbstractRequest
     private function setOptionalParams(Product $product, array $productParam): array
     {
         if ($product->getWidth() > 0) {
-            $productParam['width'] = $product->getWidth();
+            $productParam['largura'] = $product->getWidth();
         }
 
         if ($product->getHeight() > 0) {
-            $productParam['height'] = $product->getHeight();
+            $productParam['altura'] = $product->getHeight();
         }
 
         if ($product->getLength() > 0) {
-            $productParam['length'] = $product->getLength();
+            $productParam['comprimento'] = $product->getLength();
         }
 
         if ($product->getDiameter() > 0) {
-            $productParam['diameter'] = $product->getDiameter();
+            $productParam['diametro'] = $product->getDiameter();
         }
 
         if ($product->getCubicWeight() > 0) {
-            $productParam['cubicWeight'] = $product->getCubicWeight();
+            $productParam['psCubico'] = $product->getCubicWeight();
         }
 
         if ($product->getObjectType() > 1 && $product->getObjectType() <= 3) {
